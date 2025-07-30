e95f62dc553f300787f81ce11c6da1bfe021d134
Om Anghan <114564546+Omanghan11@users.noreply.github.com>
2025-07-30 10:00:54 +0530

    Fix missing key props in ProductionHouse component

--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -39,7 +39,7 @@ function ProductionHouse() {
   return (
     <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 '>
         {productionHouseList.map((item)=>(
-            <div className='border-[3px] border-gray-600
+            <div key={item.id} className='border-[3px] border-gray-600
             rounded-lg hover:scale-105 transition-all duration-300
             ease-in-out cursor-pointer relative shadow-xl 
             shadow-gray-800
