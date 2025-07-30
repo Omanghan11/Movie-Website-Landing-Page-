c0aad28ba422e508e8d614b3482b09e747161d38
Om Anghan <114564546+Omanghan11@users.noreply.github.com>
2025-07-30 10:00:22 +0530

    Fix missing key props in Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -37,19 +37,19 @@ function Header() {
         md:w-[115px] object-cover' />
         <div className='hidden md:flex gap-8 mt-5'>
         {menu.map((item)=>(
-            <HeaderItem name={item.name} Icon={item.icon} />
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
         ))}
         </div>
           <div className='flex md:hidden gap-5'>
         {menu.map((item,index)=>index<3&&(
-            <HeaderItem name={''} Icon={item.icon} />
+            <HeaderItem key={item.name} name={''} Icon={item.icon} />
         ))}
          <div className='md:hidden' onClick={()=>setToggle(!toggle)}>       
             <HeaderItem name={''} Icon={HiDotsVertical} />
            {toggle? <div className='absolute mt-3 bg-[#121212] 
             border-[1px] border-gray-700 p-3 px-5 py-4'>
             {menu.map((item,index)=>index>2&&(
-            <HeaderItem name={item.name} Icon={item.icon} />
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
             ))}
             </div>:null}
             </div> 
